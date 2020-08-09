012e7f8d30e695ed15bcdcbe5a2a08fbdd0081e6
braindead1 <2136249+braindead1@users.noreply.github.com>
2020-08-09 20:22:48 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'firebase adapter settings': {
+        'en': 'Adapter settings for firebase',
+        'de': 'Adaptereinstellungen für firebase',
+        'ru': 'Настройки адаптера для firebase',
+        'pt': 'Configurações do adaptador para firebase',
+        'nl': 'Adapterinstellingen voor firebase',
+        'fr': "Paramètres d'adaptateur pour firebase",
+        'it': "Impostazioni dell'adattatore per firebase",
+        'es': 'Ajustes del adaptador para firebase',
+        'pl': 'Ustawienia adaptera dla firebase',
+        'zh-cn': 'firebase的适配器设置'
+    },
+    'Firebase Config': {
+        'en': 'Firebase Config',
+        'de': 'Firebase-Konfiguration',
+        'ru': 'Конфигурация Firebase',
+        'pt': 'Firebase Config',
+        'nl': 'Firebase Config',
+        'fr': 'Configuration de Firebase',
+        'it': 'Firebase Config',
+        'es': 'Configuración de Firebase',
+        'pl': 'Konfiguracja Firebase',
+        'zh-cn': 'Firebase配置'
+    },
+    'Service Account': {
+        'en': 'Service Account',
+        'de': 'Dienstkonto',
+        'ru': 'Учетная запись службы',
+        'pt': 'Conta de serviço',
+        'nl': 'Serviceaccount',
+        'fr': 'Compte de service',
+        'it': 'Account di servizio',
+        'es': 'Cuenta de servicio',
+        'pl': 'Konto usługi',
+        'zh-cn': '服务帐号'
+    }
+};
---
+++ main.js
@@ -0,0 +1,123 @@
+'use strict';
+
+/*
+ * Created with @iobroker/create-adapter v1.26.0
+ */
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+const utils = require('@iobroker/adapter-core');
+
+class Firebase extends utils.Adapter {
+
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    constructor(options) {
+        super({
+            ...options,
+            name: 'firebase',
+        });
+
+        this.on('ready', this.onReady.bind(this));
+        this.on('stateChange', this.onStateChange.bind(this));
+        this.on('objectChange', this.onObjectChange.bind(this));
+        this.on('message', this.onMessage.bind(this));
+        this.on('unload', this.onUnload.bind(this));
+    }
+
+    /**
+     * Is called when databases are connected and adapter received configuration.
+     */
+    async onReady() {
+        // Reset the connection indicator during startup
+        this.setState('info.connection', false, true);
+
+        // Subscribe to changes
+        this.subscribeForeignObjects('*');
+        this.subscribeStates('*');
+
+        // Set connection indicator to conneced
+        this.setState('info.connection', true, true);
+    }
+
+    /**
+     * Is called when adapter shuts down - callback has to be called under any circumstances!
+     * @param {() => void} callback
+     */
+    onUnload(callback) {
+        try {
+            // Cleanup adapter
+
+            callback();
+        } catch (e) {
+            callback();
+        }
+    }
+
+    /**
+     * Is called if a subscribed object changes
+     * @param {string} id
+     * @param {ioBroker.Object | null | undefined} obj
+     */
+    onObjectChange(id, obj) {
+        if (obj && obj.common &&
+            (obj.common.custom &&
+                obj.common.custom[this.namespace] &&
+                typeof obj.common.custom[this.namespace] === 'object' &&
+                obj.common.custom[this.namespace].enabled)) {
+            // The object was changed
+            this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
+        } else {
+            // The object was deleted
+            //this.log.info(`object ${id} deleted`);
+        }
+    }
+
+    /**
+     * Is called if a subscribed state changes
+     * @param {string} id
+     * @param {ioBroker.State | null | undefined} state
+     */
+    onStateChange(id, state) {
+        if (state) {
+            // The state was changed
+            this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+        } else {
+            // The state was deleted
+            this.log.info(`state ${id} deleted`);
+        }
+    }
+
+    /**
+     * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
+     * Using this method requires "common.message" property to be set to true in io-package.json
+     * @param {ioBroker.Message} obj
+     */
+    onMessage(obj) {
+        if (typeof obj === 'object' && obj.message) {
+            this.log.debug(JSON.stringify(obj));
+
+            /*if (obj.command === 'send') {
+                // e.g. send email or pushover or whatever
+                this.log.info('send command');
+
+                // Send response in callback if required
+                if (obj.callback) this.sendTo(obj.from, obj.command, 'Message received', obj.callback);
+            }*/
+        }
+    }
+
+}
+
+// @ts-ignore parent is a valid property on module
+if (module.parent) {
+    // Export the constructor in compact mode
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    module.exports = (options) => new Firebase(options);
+} else {
+    // otherwise start the instance directly
+    new Firebase();
+}
